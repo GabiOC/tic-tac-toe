0e11de073b25efefeca83bd2542ad57f219de544
GabiOC <oconnor.gabriele@gmail.com>
2015-06-19 15:05:15 -0500

    Add comments

--- application.js
+++ application.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
-	makeMoves();
+	play();
 });
 
 var numMoves = 0;
 var kitty = "kitty"; var puppy = "puppy";
 var currentPlayer = kitty;
-var kittySum = 0; var puppySum = 0; // store running sum of which grids clicked
-var kittyMoves = 0; var puppyMoves = 0; // store # of moves per player
-var confirmGame;
+var kittyMoves = 0; var puppyMoves = 0; // store which grids clicked
+var kittyNumMoves = 0; var puppyNumMoves = 0; // store # of moves per player
+var confirmNewGame;
 
-// update clicked grids with markers: alternate between X's and O's
-function makeMoves(){
+function play(){
 	$(".grid").find(".grid_sq").on("click", function(){
 		if($(this).attr("clicked") != "true"){ // check if grid square is available
 			$(this).attr({
@@ -18,27 +17,25 @@ function makeMoves(){
 			});
 			if(currentPlayer == kitty){
 				$(this).append("<img width=200px height=200px src='http://thecatapi.com/api/images/get?format=src&type=gif'>");
-
-				// $(this).css({"background-color": "green"});
-				kittySum += parseInt($(this).attr("points")); // add grid score to sum for player
-				kittyMoves++; // increment player's number of moves
+				kittyMoves += parseInt($(this).attr("id")); // add grid clicked to player's moves
+				kittyNumMoves++; // increment player's number of moves
 			}
 			else {
 				$(this).append("<img width=200px height=200px src='http://www.thepuppyapi.com/puppy?format=src'>");
-				// $(this).css({"background-color": "red"});
-				puppySum += parseInt($(this).attr("points"));
-				puppyMoves++;
+				puppyMoves += parseInt($(this).attr("id"));
+				puppyNumMoves++;
 			}
 
 			gameOver(); // check if game is over
 			switchPlayer(); // if game isn't over, next turn is other player's
 
 			// // debugging
-			// console.log("kittySum " + kittySum);
 			// console.log("kittyMoves " + kittyMoves);
-			// console.log("puppySum " + puppySum);
+			// console.log("kittyNumMoves " + kittyNumMoves);
 			// console.log("puppyMoves " + puppyMoves);
+			// console.log("puppyNumMoves " + puppyNumMoves);
 			// console.log($(this).attr("clicked"));
+			// console.log(currentPlayer);
 		}
 	});
 }
@@ -46,24 +43,24 @@ function makeMoves(){
 function gameOver(){
 	var winCombos = [7, 56, 448, 73, 146, 292, 273, 84]; // grid points summed horizontally, vertically and diagonally
 
-	if(currentPlayer == kitty && kittyMoves >= 3){ // only check for winner if the player's moves are >= than 3, the earliest he/she could win
+	if(currentPlayer == kitty && kittyNumMoves >= 3){ // only check for winner if the player's moves are >= than 3, the earliest he/she could win
 		for(var i = 0; i < winCombos.length; i++){
-			if((winCombos[i] & kittySum) === winCombos[i]){
-				confirmGame = confirm("X is winner! Want to play again?");
+			if((winCombos[i] & kittyMoves) === winCombos[i]){ // use bitwise 'and' operator to check if the player's moves equal a winning combo
+				confirmNewGame = confirm("Kitties are the winner! Want to play again?");
 			}
 		}
 	}
 
-	if(currentPlayer == puppy && puppyMoves >= 3){
+	if(currentPlayer == puppy && puppyNumMoves >= 3){
 		for(var i = 0; i < winCombos.length; i++){
-			if((winCombos[i] & puppySum) === winCombos[i]){
-				confirmGame = confirm("O is winner! Want to play again?");
+			if((winCombos[i] & puppyMoves) === winCombos[i]){
+				confirmNewGame = confirm("Puppies are the winner! Want to play again?");
 			}
 		}
 	}
 	
-	if(kittyMoves + puppyMoves == 9){ // if max number of moves is reached and no winner, game is a draw
-		confirmGame = alert("Draw! Want to play again?");
+	else if(kittyNumMoves + puppyNumMoves == 9){ // if max number of moves is reached and no winner, game is a draw
+		confirmNewGame = alert("Draw! Want to play again?");
 	}
 	
 	// reset game
@@ -75,7 +72,7 @@ function switchPlayer(){
 };
 
 function newGame(){
-	if(confirmGame == true){
+	if(confirmNewGame == true){
     	window.location.reload();
 	};
 }
